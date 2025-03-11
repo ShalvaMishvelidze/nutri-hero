62fc0b73720f8a3b09cf7cf63a62e0a92c0bce5e
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-03-11 14:11:37 +0400

    made header fixed and added a spacer to keep it from overlapping the top of the content

---
+++ components/sections/header/Header.style.ts
@@ -0,0 +1,1 @@
+export const container = `bg-white fixed top-0 left-0 w-full`;
--- components/sections/header/Header.tsx
+++ components/sections/header/Header.tsx
@@ -2,14 +2,19 @@
 import { LandingHeader } from "@/components/organisms/landing_header";
 import { LoggedInHeader } from "@/components/organisms/logged_in_header";
 import { usePathname } from "next/navigation";
+import { container } from "./Header.style";
 
 const Header = () => {
   const pathname = usePathname();
 
   return (
-    <section className="bg-white">
-      {pathname === "/" ? <LandingHeader /> : <LoggedInHeader />}
-    </section>
+    <>
+      <section className={container}>
+        {pathname === "/" ? <LandingHeader /> : <LoggedInHeader />}
+      </section>
+      {/* spacer */}
+      <div className="w-full h-[80px]"></div>
+    </>
   );
 };
 export default Header;
