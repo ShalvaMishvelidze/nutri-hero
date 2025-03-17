6dd7bf3256b2c223066095c17b275891790522af
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-03-17 14:55:10 +0400

    refactored hero container component. added image  into the container itself

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { HeroContainer } from "@/components/sections/hero_container";
-import { HeroImage } from "@/components/atoms/hero_image";
 import { LandingHero } from "@/components/organisms/landing_hero";
 import { LandingExercise } from "@/components/sections/landing_exercise";
 import { Slider } from "@/components/sections/slider";
@@ -63,7 +62,6 @@ export default function Home() {
   return (
     <>
       <HeroContainer>
-        <HeroImage />
         <LandingHero />
       </HeroContainer>
       <LandingExercise />
--- components/sections/hero_container/HeroContainer.tsx
+++ components/sections/hero_container/HeroContainer.tsx
@@ -1,7 +1,13 @@
+import { HeroImage } from "@/components/atoms/hero_image";
 import { container } from "./HeroContainer.style";
 import { ReactNode } from "react";
 
 const HeroContainer = ({ children }: { children: ReactNode }) => {
-  return <div className={container}>{children}</div>;
+  return (
+    <div className={container}>
+      <HeroImage />
+      {children}
+    </div>
+  );
 };
 export default HeroContainer;
