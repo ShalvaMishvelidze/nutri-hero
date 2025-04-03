6923933a3cb67c8e4bc4d7558114743566b55255
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-04-03 12:16:18 +0400

    fixed video issues

--- components/sections/video_container/VideoContainer.style.ts
+++ components/sections/video_container/VideoContainer.style.ts
@@ -1,1 +1,5 @@
-export const container = `w-full h-[720px] relative flex justify-center`;
+export const container = `!w-full h-[720px] relative flex justify-center`;
+export const video_style =
+  "w-full h-full absolute top-0 left-0 z-1 object-cover";
+export const mini_player_style =
+  "[&]:fixed [&]:top-[120px] [&]:left-1/2 [&]:w-[409px] [&]:shadow-md [&]:z-50 [&]:h-[239px] [&]:transform [&]:translate-x-[91px] max-1000:[&]:left-auto max-1000:[&]:right-0 max-1000:[&]:translate-x-0";
--- components/sections/video_container/VideoContainer.tsx
+++ components/sections/video_container/VideoContainer.tsx
@@ -1,5 +1,9 @@
 "use client";
-import { container } from "./VideoContainer.style";
+import {
+  container,
+  mini_player_style,
+  video_style,
+} from "./VideoContainer.style";
 import { ReactNode, useEffect, useState } from "react";
 import { Items, Pb } from "./VideoContainer.config";
 import Image from "next/image";
@@ -17,67 +21,77 @@ const VideoContainer = ({
   overlay?: boolean;
   videoRef?: React.RefObject<HTMLVideoElement>;
 }) => {
+  const [scrollPosition, setScrollPosition] = useState(0);
   const [showMiniPlayer, setShowMiniPlayer] = useState(false);
+  const [miniPlayer, setMiniPlayer] = useState<"disabled" | "enabled">(
+    "enabled"
+  );
 
   useEffect(() => {
     const handleScroll = () => {
-      const video = document.getElementById("main-video");
-      if (video) {
-        const rect = video.getBoundingClientRect();
-        setShowMiniPlayer(rect.bottom < 100);
-      }
+      setScrollPosition(window.scrollY);
     };
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  useEffect(() => {
+    if (scrollPosition > 500) {
+      if (miniPlayer !== "disabled") {
+        setShowMiniPlayer(true);
+      }
+    } else {
+      setShowMiniPlayer(false);
+      setMiniPlayer("enabled");
+    }
+  }, [scrollPosition, miniPlayer]);
+
   const handleCloseMiniPlayer = () => {
     setShowMiniPlayer(false);
+    setMiniPlayer("disabled");
   };
 
-  const handleScrollToMainVideo = () => {
-    if (videoRef!.current) {
-      videoRef!.current.scrollIntoView({ behavior: "smooth" });
-    }
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
     <div className={`${container} ${Items[items]} ${Pb[pb]}`}>
-      <video
-        ref={videoRef}
-        className="w-full h-full absolute top-0 left-0 z-1 object-cover"
-        id="main-video"
-        autoPlay
-        muted
-      >
-        <source src="/video-bg.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      {showMiniPlayer && (
-        <div className="fixed top-[120px] right-[140px] w-[409px] shadow-md z-50">
-          <Image
-            src={"/scroll-into-view.svg"}
-            alt="scroll"
-            width={18}
-            height={18}
-            className="absolute top-[10px] left-[10px] cursor-pointer size-[18px] z-50"
-            onClick={handleScrollToMainVideo}
-          />
-          <Image
-            src={"/cancel.svg"}
-            alt="scroll"
-            width={18}
-            height={18}
-            className="absolute top-[10px] right-[10px] cursor-pointer size-[18px] z-50"
-            onClick={handleCloseMiniPlayer}
-          />
-          <video autoPlay muted className="w-full  object-cover">
-            <source src="/video-bg.mp4" type="video/mp4" />
-          </video>
-        </div>
-      )}
-      <div className="relative z-50 max-w-[1280px] w-full">{children}</div>
+      <div className={`${video_style} ${showMiniPlayer && mini_player_style} `}>
+        <video
+          ref={videoRef}
+          className="w-full h-full object-cover relative z-10"
+          id="main-video"
+          autoPlay
+          muted
+          onEnded={() => videoRef?.current?.play()}
+        >
+          <source src="/video-bg.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <Image
+          src={"/scroll-into-view.svg"}
+          alt="scroll"
+          width={18}
+          height={18}
+          className={`absolute top-[10px] left-[10px] cursor-pointer size-[18px] z-30 ${
+            showMiniPlayer ? "block" : "hidden"
+          }`}
+          onClick={handleScrollToTop}
+        />
+        <Image
+          src={"/cancel.svg"}
+          alt="scroll"
+          width={18}
+          height={18}
+          className={`absolute top-[10px] right-[10px] cursor-pointer size-[18px] z-30 ${
+            showMiniPlayer ? "block" : "hidden"
+          }`}
+          onClick={handleCloseMiniPlayer}
+        />
+      </div>
+      <div className="relative z-40 max-w-[1280px] w-full">{children}</div>
       {overlay && (
         <div className="bg-gray-15 absolute left-0 top-0 w-full h-full z-10"></div>
       )}
