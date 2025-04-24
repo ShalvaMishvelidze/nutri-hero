08b0dd16af601d7b99006f31442941b3231cb6e2
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-04-24 10:41:51 +0400

    improved footer style

--- components/organisms/footer_nav_container/FooterNavContainer.tsx
+++ components/organisms/footer_nav_container/FooterNavContainer.tsx
@@ -12,10 +12,10 @@ const FooterNavContainer = () => {
     <div className={container}>
       <Nav heading="explore" links={explore_links} />
       <Nav heading="company" links={company_links} is_logged_in />
-      <div className="max-sm:hidden">
+      <div>
         <Nav heading="blog" links={blog_links} is_logged_in />
       </div>
-      <div className="max-sm:hidden">
+      <div>
         <Nav
           heading="social"
           links={social_links}
